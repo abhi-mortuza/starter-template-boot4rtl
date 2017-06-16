af7c61f2c7443b5e9c2849ed55fa40651a86e753
Abhi Mortuza <golammortuzaabhi@gmail.com>
2017-06-16 13:29:55 +0600

    v1.0.1

--- js/script.js
+++ js/script.js
@@ -1,8 +1,25 @@
-(function(){
+(function($){
+/*!
+ * IE10 viewport hack for Surface/desktop Windows 8 bug
+ * Copyright 2014-2017 The Bootstrap Authors
+ * Copyright 2014-2017 Twitter, Inc.
+ * Licensed under MIT (https://github.com/twbs/bootstrap/blob/master/LICENSE)
+ */
+
+// See the Getting Started docs for more information:
+// https://getbootstrap.com/getting-started/#support-ie10-width
+  if (navigator.userAgent.match(/IEMobile\/10\.0/)) {
+    var msViewportStyle = document.createElement('style')
+    msViewportStyle.appendChild(
+      document.createTextNode(
+        '@-ms-viewport{width:auto!important}'
+      )
+    )
+    document.head.appendChild(msViewportStyle)
+  }
 	/*********************************************
 	*************
 	** Scroll to top
-	**
 	*************
 	**********************************************/
 	var amountScrolled = 300;
@@ -20,4 +37,4 @@
       return false;
     });
 
-})();
+})(jQuery);
